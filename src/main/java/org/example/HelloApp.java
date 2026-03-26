70e9a0356a5b333f93d2f15f87a00d2c24cf9372
next-0508 <kjose0508@gmail.com>
2026-03-26 08:53:19 +0530

    [Kevin]added display name with default value

--- src/main/java/org/example/HelloApp.java
+++ src/main/java/org/example/HelloApp.java
@@ -5,12 +5,9 @@ package org.example;
 public class HelloApp {
     public static void main(String[] args) {
 
-        String name = "";
-        try {
-            name = args[0];
-        }
-        catch(Exception e){
-            System.out.println(e.getMessage());
+        String name = "World";
+        if (args.length >0){
+            name= args[0];;
         }
         System.out.println("Hello, " + name + "!");
     }
