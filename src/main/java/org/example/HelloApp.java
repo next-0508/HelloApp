89f41828ff41afb91a3300ea5105ab995a66a820
next-0508 <kjose0508@gmail.com>
2026-04-05 20:49:13 +0530

    [Kevin]added : UC7

--- src/main/java/org/example/HelloApp.java
+++ src/main/java/org/example/HelloApp.java
@@ -8,20 +8,12 @@ public class HelloApp {
         // Default case: no arguments
         if (args.length == 0) {
             System.out.println("Hello, World!");
-            return;
+        } else {
+            // Join names with comma and space
+            String names = String.join(", ", args);
+
+            // Print greeting
+            System.out.println("Hello, " + names + "!");
         }
-
-        String names = "";
-
-        // Enhanced for-loop to build string
-        for (String name : args) {
-            names += name + ", ";
-        }
-
-        // Remove trailing ", " using substring
-        names = names.substring(0, names.length() - 2);
-
-        // Print greeting
-        System.out.println("Hello, " + names + "!");
     }
 }
